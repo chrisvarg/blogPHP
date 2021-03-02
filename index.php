50b6d875fdbba73611aea0d8ae0d36e3031b8467
ChrisVar <ing.christiand.vargas@gmail.com>
2021-03-01 22:44:29 -0500

    Inicio header
    Estructura del header en el HTML

--- index.php
+++ index.php
@@ -10,7 +10,40 @@
     </head>
     <body>
         <!-- HEADER -->
-        <!-- MENU -->
+        <header class="site-header">
+            <!-- LOGO -->
+            <div class="header-logo">
+                <a href="index.php">
+                    Blog de Videojuegos
+                </a>
+            </div>
+            <!-- MENU -->
+            <nav class="header-nav">
+                <ul>
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 4</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Sobre mi</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
         <!-- SIDERBAR -->
         <!-- MAIN -->
         <!-- FOOTER -->
