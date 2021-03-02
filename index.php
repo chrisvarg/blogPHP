b0303131d0170c28b034ab1bced1db5ac60d60b4
ChrisVar <ing.christiand.vargas@gmail.com>
2021-03-01 22:34:47 -0500

    Inicio del proyecto :3

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Mi primer blog con php"/>
+        <meta name="robots" content="follow,index.php"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blog de Videojuegos</title>
+    </head>
+    <body>
+        <!-- HEADER -->
+        <!-- MENU -->
+        <!-- SIDERBAR -->
+        <!-- MAIN -->
+        <!-- FOOTER -->
+    </body>
+</html>
