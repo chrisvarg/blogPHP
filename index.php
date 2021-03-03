afe70ff1ba91b58dcb797a142295d0cb73089f1d
ChrisVar <ing.christiand.vargas@gmail.com>
2021-03-02 19:39:25 -0500

    Estilos del header y correcciones en el html

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         <meta name="robots" content="follow,index.php"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Blog de Videojuegos</title>
+        <link rel="stylesheet" href="./assets/css/main.css" type="text/css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
     </head>
     <body>
         <!-- HEADER -->
@@ -19,30 +22,31 @@
             </div>
             <!-- MENU -->
             <nav class="header-nav">
-                <ul>
-                    <li>
+                <ul class="nav-menu">
+                    <li class="nav-menu__items">
                         <a href="index.php">Inicio</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Categoria 1</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Categoria 2</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Categoria 3</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Categoria 4</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Sobre mi</a>
                     </li>
-                    <li>
+                    <li class="nav-menu__items">
                         <a href="index.php">Contacto</a>
                     </li>
                 </ul>
             </nav>
+            <div class="clearfix"></div>
         </header>
         <!-- SIDERBAR -->
         <!-- MAIN -->
